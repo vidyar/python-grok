058d6b2688417ca7907f8c491065ae067aa056ec
Spencer Herzberg <spencer.herzberg@gmail.com>
2013-07-06 13:34:19 -0500

    changing add pattern from file to be more intuitive

--- grok/__init__.py
+++ grok/__init__.py
@@ -18,10 +18,9 @@ class Grok(object):
     def add_patterns_from_file(self, file):
         with open(file, 'r') as fh:
             for line in fh.readlines():
-                if re.compile(r'^(\s*#\s*|\s*$)').match(line):
-                    continue
-                name, pattern = re.compile(r'\s+').split(line.lstrip(), 1)
-                self.add_pattern(name, pattern.strip())
+                if re.compile(r'^[a-zA-Z0-9_]+\s.*$').match(line):
+                    name, pattern = re.compile(r'\s+').split(line.lstrip(), 1)
+                    self.add_pattern(name.strip(), pattern.strip())
 
     def compile(self, pattern):
         pass
